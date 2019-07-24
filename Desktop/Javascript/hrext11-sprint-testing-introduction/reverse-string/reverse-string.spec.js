5679fde921bd6342c73098c82b89f3c578e9940b
Daylan <daylanberry@gmail.com>
2019-07-24 11:01:52 -0700

    Added additional test cases for inputs, 'cat' and ''

---
+++ Desktop/Javascript/hrext11-sprint-testing-introduction/reverse-string/reverse-string.spec.js
@@ -0,0 +1,17 @@
+describe('reverseString', function() {
+  it('reverses a string', function() {
+    expect(reverseString('never')).to.equal('reven');
+  });
+});
+
+describe('reverseString', function(){
+	it('reverses a string', function(){
+		expect(reverseString('cat').to.equal('tac'))
+	});
+});
+
+describe('reverseString', function(){
+	it('reverses a string', function(){
+		expect(reverseString('').to.equal(''))
+	})
+})
